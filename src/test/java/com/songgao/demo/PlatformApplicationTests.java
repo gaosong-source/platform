fe64d513c6e5a092af12e4e54b5c83ade94c4260
‘gaosong’ <gaosong@zjugis.com>
2020-02-14 22:41:54 +0800

    新建项目

---
+++ src/test/java/com/songgao/demo/PlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songgao.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
